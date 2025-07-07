32368a2957ccba863b9f490337b9f0b298037136
MRV-dev <a.marv0903@gmail.com>
2025-07-07 10:53:05 +0800

    itemcard

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,9 @@ import 'package:flutter/material.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
 
