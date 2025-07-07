c2a195a1b99804aa7d2a10785c7936737d689c8a
MRV-dev <a.marv0903@gmail.com>
2025-07-07 10:33:29 +0800

    itemcard

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:new_app/Home.dart';
-import 'package:new_app/pages/ListItems.dart';
-import 'package:new_app/pages/forms.dart';
+
 
 
 void main() {
@@ -9,16 +7,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/' : (context) => listItems(),
-        '/Add' : (context) => AddWorkout(),
-      },
-      debugShowCheckedModeBanner: false,
-    );
+    return const Placeholder();
   }
 }
+
