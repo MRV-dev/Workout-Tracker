3e452bd77d04f04ee6169efe6c94a358fcef8958
MRV-dev <a.marv0903@gmail.com>
2025-07-07 22:59:44 +0800

    itemcard

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/World_time/choose_location.dart';
 import 'package:new_app/World_time/home.dart';
+import 'package:new_app/World_time/loading.dart';
 
 
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home(),
+    routes: {
+      '/' : (context) => Home(),
+      '/loading' : (context) => Loading(),
+      '/location' : (context) => ChooseLocation(),
+    },
   ));
 }
 
